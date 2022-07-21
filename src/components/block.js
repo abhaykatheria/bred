5f4955e4df285d193c9e53157f80b3901d8bed61
abhaykatheria <abhay.katheria1998@gmail.com>
2022-07-21 09:58:06 +0530

    First commit

---
+++ src/components/block.js
@@ -0,0 +1,16 @@
+class Block extends Component {
+
+    constructor(props) {
+        super(props);
+      }
+
+    state = {}
+
+    render() { 
+        return (
+            <h1></h1>
+        );
+    }
+}
+ 
+export default Block;
