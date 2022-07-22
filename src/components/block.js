5c6a0e414c7d3f85c9669f82fc9ef78071369e03
abhaykatheria <abhay.katheria1998@gmail.com>
2022-07-22 13:00:27 +0530

    dfdg

--- src/components/block.js
+++ src/components/block.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-// import './main.css';
-import Phone from '../components/phone';
+
 
 class Block extends Component {
 
@@ -15,21 +13,17 @@ class Block extends Component {
 
     render() {
         return (
-            <Row style={{height:'100vh'}}>
-                <Col sm={8} style={{display : 'flex' , alignItems:'center'}}>
+                    <Row style={{height:'100vh',  display:'flex', alignItems:"center"}}>
                     <Container className='left-col' fluid>
                         <h1 style={{ fontSize: '100px', marginLeft: '50px' }}>
                             {this.props.title}
                         </h1>
-                        <br></br>
-                        <br></br>
+                        
                         <h3 style={{ fontSize: '40px', marginLeft: '50px' }}>
                             {this.props.content}
                         </h3>
                     </Container>
-                </Col>
-                <Col sm={4} style={{display : 'flex' , alignItems:'center'}}><Phone url={this.props.img}></Phone></Col>
-            </Row>
+                    </Row>
         );
     }
 }
