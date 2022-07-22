03bb186d956c79f823eb3ce8a62c7829930aabcc
abhaykatheria <abhay.katheria1998@gmail.com>
2022-07-22 13:00:19 +0530

    working

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
 import './main.css';
 import Block from '../components/block';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// import './main.css';
+import Phone from '../components/phone';
 
 export default class Main extends Component {
     state = { active: 0 };
@@ -16,40 +19,53 @@ export default class Main extends Component {
     }
 
     handleScroll = event => {
-        const { pageYOffset } = window;
+        const { pageYOffset,innerHeight } = window;
         const { active } = this.state;
-
-        if (pageYOffset >= 500 && active === 0) {
-            this.setState({ active: 1 });
-        } else if (pageYOffset < 500 && active === 1) {
-            this.setState({ active: 0 });
-        }
+        console.log(innerHeight)
+        console.log(pageYOffset)
+        this.setState({active : (pageYOffset-(pageYOffset%innerHeight))/innerHeight})
     };
 
 
 
-
+    urls = [
+        "https://web-images.credcdn.in/_next/assets/images/home-page/features/fold1.png",
+        "https://web-images.credcdn.in/_next/assets/images/home-page/features/fold2.png",
+        "https://web-images.credcdn.in/_next/assets/images/home-page/features/fold3.png",
+        "https://web-images.credcdn.in/_next/assets/images/home-page/features/fold4.png"
+    ]
     render() {
-        // const { active } = this.state;
-        // urls = [
-        //     "https://web-images.credcdn.in/_next/assets/images/home-page/features/fold3.png",
-        //     "https://web-images.credcdn.in/_next/assets/images/home-page/features/fol2.png"
-        // ]
+        const { active } = this.state;
+        
         return (
-            <Container className='Main' fluid>
-
-                <Block
-                    title="This is block 1"
-                    content="It took very long time to work"
-                    img="https://web-images.credcdn.in/_next/assets/images/home-page/features/fold2.png">
-                </Block>
-
-                <Block
-                    title="This is block 2"
-                    content="It took very long time to work"
-                    img="https://web-images.credcdn.in/_next/assets/images/home-page/features/fold3.png">
-                </Block>
-            </Container>
+            
+                <div className="container-fluid" >
+                    <div className="row" >
+                    <div className="col-sm-6 col-2 two">
+                        <Block
+                        title = "My fist block"
+                        content = "lets roll bitches"
+                        ></Block>
+                        <Block
+                        title = "My fist block"
+                        content = "lets roll bitches"
+                        ></Block> 
+                        <Block
+                        title = "My fist block"
+                        content = "lets roll bitches"
+                        ></Block>
+                        <Block
+                        title = "My fist block"
+                        content = "lets roll bitches"
+                        ></Block>
+                        
+                    </div>
+                        <div className="col-sm-4 offset-sm-8 fixed-top one" style={{display:"flex", top:"15vh"}}>
+                            <Phone url ={this.urls[active]}></Phone>
+                      </div>
+                      
+                </div>
+            </div>
         )
     }
 }
